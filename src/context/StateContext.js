99b93ed3fc65c548dc9aa569ce805c636771504b
Mahamed Mahad <mahameddeeq26@hotmail.com>
2022-06-09 13:29:50 +0200

    Add items to the add functionality

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -11,6 +11,38 @@ export const StateContext = ({ children }) => {
   const [totalQuantities, setTotalQuantities] = useState()
   const [qty, setQty] = useState(1)
 
+  const onAddCart = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    )
+
+    /* A function that takes the previous total price and adds the product price multiplied by the
+quantity. */
+    setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity)
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity)
+
+    /* This is checking if the product is already in the cart. If it is, it will update the quantity of
+    the product. */
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProduct) => {
+        if (cartProduct._id === product._id)
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + quantity,
+          }
+      })
+      setCartItems(updatedCartItems)
+    } else {
+
+    /* If the product is not in the cart, it will add the product to the cart. */
+      product.quantity = quantity
+
+      setCartItems([...cartItems, { ...product }])
+    }
+
+    toast.success(`${qty} ${product.name} added to cart`)
+  }
+
   const incQty = () => {
     setQty((prevQty) => prevQty + 1)
   }
