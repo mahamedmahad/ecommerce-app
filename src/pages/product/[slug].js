1a2ffbe400187d0d4484baa4d8e3233796031beb
Mahamed Mahad <mahameddeeq26@hotmail.com>
2022-06-09 12:42:26 +0200

    similar products with animation

--- src/pages/product/[slug].js
+++ src/pages/product/[slug].js
@@ -9,65 +9,81 @@ import {
   AiOutlineStar,
 } from 'react-icons/ai'
 
+import { Product } from '../../components'
+
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product
 
   // console.log(products)
 
   return (
-    <div className='product-detail-container'>
-      <div>
-        <div className='image-container'>
-          <img src={urlFor(image && image[0])} />
-        </div>
+    <div>
+      <div className='product-detail-container'>
+        <div>
+          <div className='image-container'>
+            <img src={urlFor(image && image[0])} />
+          </div>
 
-        {/* <div className='small-images-container'>
+          {/* <div className='small-images-container'>
           {image?.map((item, i) => (
             <img src={urlFor(item)} key={i} className='' onMouseEnter={''} />
           ))}
         </div> */}
-      </div>
+        </div>
 
-      <div className='product-detail-desc'>
-        <h1>{name}</h1>
-        <div className='reviews'>
-          <div>
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiFillStar />
-            <AiOutlineStar />
+        <div className='product-detail-desc'>
+          <h1>{name}</h1>
+          <div className='reviews'>
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+
+            <p>(20)</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className='price'>${price}</p>
+
+          {/* quantity */}
+          <div className='quantity'>
+            <h3>Quantity</h3>
+            <p className='quantity-desc'>
+              <span className='minus' onClick={''}>
+                <AiOutlineMinus />
+              </span>
+              <span className='num' onClick={''}>
+                0
+              </span>
+              <span className='plus' onClick={''}>
+                <AiOutlinePlus />
+              </span>
+            </p>
           </div>
 
-          <p>(20)</p>
+          <div className='buttons'>
+            <button type='button' className='add-to-cart'>
+              Add to Cart
+            </button>
+            <button type='button' className='buy-now'>
+              Buy Now
+            </button>
+          </div>
         </div>
-        <h4>Details:</h4>
-        <p>{details}</p>
-        <p className='price'>${price}</p>
+      </div>
 
-        {/* quantity */}
-        <div className='quantity'>
-          <h3>Quantity</h3>
-          <p className='quantity-desc'>
-            <span className='minus' onClick={''}>
-              <AiOutlineMinus />
-            </span>
-            <span className='num' onClick={''}>
-              0
-            </span>
-            <span className='plus' onClick={''}>
-              <AiOutlinePlus />
-            </span>
-          </p>
-        </div>
-
-        <div className='buttons'>
-          <button type='button' className='add-to-cart'>
-            Add to Cart
-          </button>
-          <button type='button' className='buy-now'>
-            Buy Now
-          </button>
+      {/* similar products */}
+      <div className='maylike-products-wrapper'>
+        <h2>You may also like</h2>
+        <div className='marquee'>
+          <div className='maylike-products-container track'>
+            {products?.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
