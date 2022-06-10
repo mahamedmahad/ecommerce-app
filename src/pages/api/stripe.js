edace8801249acddc580ae718d66240d2ccb02e1
Mahamed Mahad <mahameddeeq26@hotmail.com>
2022-06-10 15:11:58 +0200

    Pay-with-stripe functionality!

--- src/pages/api/stripe.js
+++ src/pages/api/stripe.js
@@ -4,9 +4,18 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
+    // console.log(req.body)
     try {
       // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create({
+      const params = {
+        submit_type: 'pay',
+        mode: 'payment',
+        payment_method_types: ['card'],
+        billing_address_collection: 'auto',
+        shipping_options: [
+          { shipping_rate: 'shr_1L977EAMN45FsYaWZ3W9y4DU' },
+          { shipping_rate: 'shr_1L978VAMN45FsYaWFXwUcnKc' },
+        ],
         line_items: [
           {
             // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
@@ -14,11 +23,15 @@ export default async function handler(req, res) {
             quantity: 1,
           },
         ],
-        mode: 'payment',
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
-      })
-      res.redirect(303, session.url)
+      }
+
+      const session = await stripe.checkout.sessions.create(params)
+
+      res.status(200).json(session)
+
+      // res.redirect(303, session.url)
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message)
     }
