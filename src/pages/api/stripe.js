a9420fd8fc97e3a54640bba9b7150521fdf65540
Mahamed Mahad <mahameddeeq26@hotmail.com>
2022-06-10 20:24:16 +0200

    Successfully added stripe Transaction

--- src/pages/api/stripe.js
+++ src/pages/api/stripe.js
@@ -1,6 +1,6 @@
 import Stripe from 'stripe'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY)
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -14,15 +14,44 @@ export default async function handler(req, res) {
         billing_address_collection: 'auto',
         shipping_options: [
           { shipping_rate: 'shr_1L977EAMN45FsYaWZ3W9y4DU' },
-          { shipping_rate: 'shr_1L978VAMN45FsYaWFXwUcnKc' },
+          // { shipping_rate: 'shr_1L979QAMN45FsYaW7ziMA72R' },
         ],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
+        // cartItems: req.body,
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref
+
+          const newImg = img
+            .replace(
+              'image-',
+              'https://cdn.sanity.io/images/ok82ra8q/production/'
+            )
+            .replace('-webp', '.webp')
+
+          // console.log('MAGE', newImg)
+
+          return {
+            price_data: {
+              currency: 'usd',
+              product_data: {
+                name: item.name,
+                images: [newImg],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled: true,
+              minimum: 1,
+            },
+            quantity: item.quantity,
+          }
+        }),
+        // line_items: [
+        //   {
+        //     // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+        //     price: '{{PRICE_ID}}',
+        //     quantity: 1,
+        //   },
+        // ],
         success_url: `${req.headers.origin}/?success=true`,
         cancel_url: `${req.headers.origin}/?canceled=true`,
       }
